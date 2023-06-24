976ef9ab38c435bb4e36dab340e7be22ffe08f80
halangbacca <halanbacca@outlook.com>
2023-06-24 12:29:16 -0300

    Ex 1 - Criar uma Função para calcular desconto

---
+++ Exercícios M3-S08.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Produto;
+SELECT * FROM ProdutoPreco;
+
+-- Exercício 1
+CREATE OR REPLACE FUNCTION CalcularDesconto(IdSelecionado NUMBER, desconto NUMBER) RETURN NUMBER IS 
+BEGIN
+	FOR produto IN (SELECT Valor FROM ProdutoPreco WHERE ID = IdSelecionado) LOOP
+		IF(desconto = 1) THEN
+			RETURN produto.valor - (produto.valor * 0.10);
+		ELSE
+			RETURN produto.valor;
+		END IF;
+	END LOOP;
+END;
+
+SELECT Valor AS VALOR_TOTAL, CalcularDesconto(ID, 1) AS VALOR_COM_DESCONTO FROM ProdutoPreco WHERE ID = 5;
